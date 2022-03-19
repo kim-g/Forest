f0c71ea1e95b1783b8427f15c976f2ba9f460ae9
kim-g <kim-g@yandex.ru>
2022-03-19 13:28:29 +0500

    Revert "."
    This reverts commit 5346efad020d3f831a27d24e15ed3dec63266096.

--- Forest/Forest.py
+++ Forest/Forest.py
@@ -7,20 +7,19 @@ import Animals
 import ProgramInterface
 import math
 
-
-def CreateBeast(x: float, y: float, ax: float, ay: float):
+def CreateBeast(x:float, y:float, ax:float, ay:float):
     """Функция для создания абстрактного зверя для отладки"""
     # Создание Зверя
     Beast = Animals.Beast()
-    Beast.Position = np.array([x, y])
-    Beast.Aim = np.array([ax, ay])
+    Beast.Position = np.array([x,y])
+    Beast.Aim = np.array([ax,ay])
     Beast.Speed = 1.
     Beast.EcoSystem = Env
     Beast.Stamina = 10
     Beast.Digested_Per_Step = 0.
     Beast.Biomass = 10.
     Beast.EnergyPerStep = 0.
-    # Env.Ground[500,500].append(animal)
+    #Env.Ground[500,500].append(animal)
     Env.Elements.append(Beast)
     Env.Alive.append(Beast)
 
@@ -30,8 +29,7 @@ def CreateBeast(x: float, y: float, ax: float, ay: float):
     animals_sprites.add(Beast)
     interface_sprites.add(Beast._aim_sprite)
 
-
-def CreateFox(x: float, y: float, ax: float, ay: float):
+def CreateFox(x:float, y:float, ax:float, ay:float):
     """Функция для создания песца"""
     Fox = Animals.Fox()
     Fox.Position = np.array([x, y])
@@ -52,8 +50,7 @@ def CreateFox(x: float, y: float, ax: float, ay: float):
     animals_sprites.add(Fox)
     interface_sprites.add(Fox._aim_sprite)
 
-
-def CreateTurtle(x: float, y: float, ax: float, ay: float):
+def CreateTurtle(x:float, y:float, ax:float, ay:float):
     """Функция для создания черепахи для отладки"""
     Turtle = Animals.Turtle()
     Turtle.Position = np.array([x, y])
@@ -76,8 +73,7 @@ def CreateTurtle(x: float, y: float, ax: float, ay: float):
     animals_sprites.add(Turtle)
     interface_sprites.add(Turtle._aim_sprite)
 
-
-def CreateChameleon(x: float, y: float, ax: float, ay: float):
+def CreateChameleon(x:float, y:float, ax:float, ay:float):
     """Функция для создания хамелеона для отладки"""
     Chameleon = Animals.Chameleon()
     Chameleon.Position = np.array([x, y])
@@ -96,7 +92,6 @@ def CreateChameleon(x: float, y: float, ax: float, ay: float):
     animals_sprites.add(Chameleon)
     interface_sprites.add(Chameleon._aim_sprite)
 
-
 # Функция, создающая кролика из Террарии
 def CreateBunny(x, y, ax, ay):
     # Создание кролика
@@ -109,7 +104,7 @@ def CreateBunny(x, y, ax, ay):
     Bunny.Digested_Per_Step = 15.
     Env.Elements.append(Bunny)
     Env.Alive.append(Bunny)
-    Bunny.Biomass = 25.
+    Bunny.Biomass = 2500.
     Bunny.EatenBiomass = 1
     Bunny.EatenBiomassTreshold = 50.
     Bunny.Eaten_Biomass_Lower_Treshold = 10.
@@ -122,7 +117,6 @@ def CreateBunny(x, y, ax, ay):
     animals_sprites.add(Bunny)
     interface_sprites.add(Bunny._aim_sprite)
 
-
 def CreateWolf(x, y, ax, ay):
     # Создание волка
     Wolf = Animals.Wolf()
@@ -134,7 +128,7 @@ def CreateWolf(x, y, ax, ay):
     Wolf.Digested_Per_Step = 15.
     Env.Elements.append(Wolf)
     Env.Alive.append(Wolf)
-    Wolf.Biomass = 25.
+    Wolf.Biomass = 2500.
     Wolf.EatenBiomass = 1
     Wolf.EatenBiomassTreshold = 50.
     Wolf.Eaten_Biomass_Lower_Treshold = 10.
@@ -147,16 +141,15 @@ def CreateWolf(x, y, ax, ay):
     animals_sprites.add(Wolf)
     interface_sprites.add(Wolf._aim_sprite)
 
-
 # Функция, создающая траву
-def CreateGrass(Parent=None):
+def CreateGrass(Parent = None):
     """Функция для создания травы"""
     Grass = Plants.Grass()
     if Parent == None:
         Grass.X = random.randint(0, Env.Width - 1)
         Grass.Y = random.randint(0, Env.Height - 1)
     else:
-        angle = random.randint(0, 360)
+        angle = random.randint(0,360)
         angle = float(angle / 360 * 2 * math.pi)
         Grass.X = Parent.X + math.cos(angle)
         Grass.Y = Parent.Y + math.sin(angle)
@@ -174,12 +167,11 @@ def CreateGrass(Parent=None):
     all_sprites.add(Grass)
     plants_sprites.add(Grass)
 
-
 # Визуализация
 FPS = 15
 pygame.init()
-#pygame.mixer.init()  # для звука
-screen = pygame.display.set_mode((0, 0))
+pygame.mixer.init()  # для звука
+screen = pygame.display.set_mode((0,0))
 pygame.display.set_caption("Forest")
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
@@ -202,34 +194,21 @@ for i in range(0, 10):
 for i in range(0, 400):
     CreateGrass()
 
+
 # Создание окна интерфейса
 Pause = ProgramInterface.PauseWindow()
-Pause.Show = False
+Pause.Show = False;
 interface_sprites.add(Pause)
 all_sprites.add(Pause)
 
 StepsCountWindow = ProgramInterface.StepsWindow()
 interface_sprites.add(StepsCountWindow)
 all_sprites.add(StepsCountWindow)
-
-BiomassCount = ProgramInterface.BiomassCount()
-interface_sprites.add(BiomassCount)
-all_sprites.add(BiomassCount)
-
-BiomassCountAnimal = ProgramInterface.BiomassCountAnimal()
-interface_sprites.add(BiomassCountAnimal)
-all_sprites.add(BiomassCountAnimal)
-
-BiomassCountPlants = ProgramInterface.BiomassCountPlants()
-interface_sprites.add(BiomassCountPlants)
-all_sprites.add(BiomassCountPlants)
+StepsCountWindow.Count = 5326
 
 running = True
 interface = True
 pause = False
-
-steps = 0
-
 
 while running:
     # Ограничение FPS
@@ -257,34 +236,21 @@ while running:
                 pause = not pause
                 Pause.Show = pause
 
-    #            if event.key == pygame.K_t:
-    #                CreateTurtle(23., 32., 0., 0.)
-    #            if event.key == pygame.K_f:
-    #                CreateFox(32., 23., 0., 0.)
-    #            if event.key == pygame.K_b:
-    #                CreateBeast(40., 15., 0., 0.)
+#            if event.key == pygame.K_t:
+#                CreateTurtle(23., 32., 0., 0.)
+#            if event.key == pygame.K_f:
+#                CreateFox(32., 23., 0., 0.)
+#            if event.key == pygame.K_b:
+#                CreateBeast(40., 15., 0., 0.)
 
     # Обновление
-
     if pause:
         interface_sprites.update()
     else:
         all_sprites.update()
         StepsCountWindow.Count += 1
-        biomas = 0.
-        anim_biomass = 0.
-        plants_biomass = 0.
-        for x in Env.Alive:
-            biomas += x.Biomass
-            print(type(x))
-            if str(type(x)) in ["Beast","Turtle","Fox","Wolf","Chameleon","Bunny"]:
-                anim_biomass += x.Biomass
-            if str(type(x)) == "Grass":
-                plants_biomass+=x.Biomass
-        BiomassCount.All_Bio_Count = float(round(biomas))
-        print(anim_biomass,plants_biomass)
-
-
+    #CreateGrass()
+    
 
     # Визуализация (сборка)
     screen.fill(0x00FF00)
@@ -296,5 +262,7 @@ while running:
     # после отрисовки всего, переворачиваем экран
     pygame.display.flip()
     pass
+
+
 
 pygame.quit()
